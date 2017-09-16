f986193e9cf3df878a3e87aba50a3db174bb2769
David Chen <qydchen@gmail.com>
2017-09-16 17:51:10 -0400

    do question 1

--- read_data.py
+++ read_data.py
@@ -3,23 +3,25 @@ from datetime import datetime
 from datetime import timedelta
 
 class ReadData:
-    def __init__(self, start_file, end_file=None):
+    def __init__(self, start_file='091417', end_file=datetime.today().strftime("%m%d%y")):
+        # will by default run from the first day this program started gathering data to today
         self.start_file = start_file
         self.end_file = end_file
         self.reader = {}
-        self.id_count = 1
+        self.results = {'brand_ownership': {}} # memoize results here
+        self.id_count = 0 # used to implement id across multiple files and counter for all rows
         self.init_file_read()
 
     def init_file_read(self):
         if self.end_file is not None:
-            counter = self.days_between(self.start_file, self.end_file)
+            total_days = self.days_between(self.start_file, self.end_file)
             add_day = 0
-            while add_day <= counter:
+            while add_day <= total_days:
                 filename = self.fetch_file(self.start_file, add_day)
                 self.read_file(filename)
                 add_day += 1
-        else:
-            return self.read_file(self.start_file)
+        else: # if the user input None, only run a file for one day
+            self.read_file(self.start_file)
 
     def read_file(self, filename):
         with open('./data/{0}'.format(filename)) as csvfile:
@@ -31,11 +33,37 @@ class ReadData:
     def view(self):
         print self.reader
 
+    def count(self):
+        print self.id_count
+
+    def brand_ownership(self, search_param): # for question 1
+        if search_param in self.results['brand_ownership']:
+            print self.results['brand_ownership'][search_param]
+        else:
+            brands = {}
+            search_result = {}
+            counter = 0.0
+            for item in self.reader:
+                search_term = self.reader[item][1]
+                brand = self.reader[item][2]
+                if (search_term == search_param):
+                    if ((brand not in brands)):
+                        brands[brand] = 1.0
+                        counter += 1.0
+                    else:
+                        brands[brand] += 1.0
+                        counter += 1.0
+            for brand in brands:
+                percent = "{0:.0f}%".format(brands[brand]/counter * 100)
+                search_result[brand] = percent
+            self.results['brand_ownership'][search_param] = search_result
+            print self.results['brand_ownership'][search_param]
+
     # parses the date into a file name
     def fetch_file(self, date, incre):
         datetime_obj = datetime.strptime(date, '%m%d%y') + timedelta(days=incre)
-        incremented_day = datetime_obj.strftime("%m%d%y")
-        return incremented_day
+        new_filename = datetime_obj.strftime('%m%d%y')
+        return new_filename
 
     # calculate the days between to gather the files
     def days_between(self, d1, d2):
@@ -43,5 +71,31 @@ class ReadData:
         d2 = datetime.strptime(d2, '%m%d%y')
         return abs((d2 - d1).days)
 
-newRead = ReadData('091517','091717')
-newRead.view()
+three_days = ReadData('091417','091617')
+three_days.brand_ownership('smart_tv')
+three_days.brand_ownership('curved_smart_tv')
+three_days.view()
+three_days.count()
+
+one_day = ReadData('091617', None)
+three_days.brand_ownership('smart_tv')
+one_day.brand_ownership('curved_smart_tv')
+one_day.view()
+one_day.count()
+
+# An example view:
+# {
+#     0: ['1', 'smart_tv', 'LG', '4.6', '274'],
+#     1: ['2', 'smart_tv', 'Samsung', '4.6', '5899'],
+#     2: ['3', 'smart_tv', 'LG', '4.6', '187'],
+#     3: ['4', 'smart_tv', 'Samsung', '4.6', '3318'],
+#     4: ['5', 'smart_tv', 'Samsung', '4.7', '219'],
+#     5: ['6', 'smart_tv', 'Samsung', '4.6', '2538'],
+#     6: ['7', 'smart_tv', 'LG', '4.8', '51'],
+#     7: ['8', 'smart_tv', 'LG', '4.6', '231'],
+#     8: ['9', 'smart_tv', 'LG', '4.5', '443'],
+#     9: ['10', 'smart_tv', 'Samsung', '4.7', '360'],
+#     10: ['11', 'smart_tv', 'LG', '4.5', '130'],
+#     11: ['12', 'smart_tv', 'Samsung', '4.6', '2052'],
+#     12: ['13', 'smart_tv', 'Samsung', '4.5', '99']
+# }
--- run_scrape.py
+++ run_scrape.py
@@ -4,9 +4,6 @@ import os
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-def parse_title(title):
-    return title.split()[0]
-
 smart_tv = 'https://www.bestbuy.com/site/searchpage.jsp?cp=1&searchType=search&st=smart%20tv&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=category_facet%3DSAAS~All%20Flat-Panel%20TVs~abcat0101001%5Ebrand_facet%3DBrand~Samsung%5Ebrand_facet%3DBrand~LG%5Ebrand_facet%3DBrand~Sony%5Ebrand_facet%3DBrand~Toshiba&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys'
 curved_smart_tv = 'https://www.bestbuy.com/site/searchpage.jsp?cp=1&searchType=search&st=curved%20smart%20tv&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=brand_facet%3DBrand~Samsung&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys'
 
@@ -26,12 +23,16 @@ class BestBuyScrape:
         file_name = datetime.today().strftime("%m%d%y") # filenames will represent the day it was scraped
         list_items = scrape.find('div', attrs={'class': 'list-items'})
 
-        if not os.path.exists('data'):
+        if not os.path.exists('data'): # if there's no data folder, make one
             os.makedirs('data')
         with open('./data/{0}'.format(file_name), 'a') as csv_file:
             writer = csv.writer(csv_file)
             for (idx, child) in enumerate(list_items.children):
-                writer.writerow([idx + 1, self.name, parse_title(child[title]), child[rating], child[review],])
+                # idx + 1 is used to denote ranking for each day
+                writer.writerow([idx + 1, self.name, self.parse_title(child[title]), child[rating], child[review],])
+
+    def parse_title(self, title):
+        return title.split()[0]
 
 import_smart_tv = BestBuyScrape(smart_tv, 'smart_tv')
 import_smart_tv.scrape()
