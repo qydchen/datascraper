3518762831970947e773a40e6191aba9ea7a5904
David Chen <qydchen@gmail.com>
2017-09-16 13:53:41 -0400

    DRY code and gather data

--- search_result.py
+++ search_result.py
@@ -1,4 +1,7 @@
 import urllib2
+import csv
+import os
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 def parse_title(title):
@@ -7,102 +10,29 @@ def parse_title(title):
 smart_tv = 'https://www.bestbuy.com/site/searchpage.jsp?cp=1&searchType=search&st=smart%20tv&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=category_facet%3DSAAS~All%20Flat-Panel%20TVs~abcat0101001%5Ebrand_facet%3DBrand~Samsung%5Ebrand_facet%3DBrand~LG%5Ebrand_facet%3DBrand~Sony%5Ebrand_facet%3DBrand~Toshiba&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys'
 curved_smart_tv = 'https://www.bestbuy.com/site/searchpage.jsp?cp=1&searchType=search&st=curved%20smart%20tv&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=brand_facet%3DBrand~Samsung&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys'
 
-def scrape_bestbuy(url):
-    rating = 'data-average-rating'
-    review = 'data-review-count'
-    title = 'data-title'
-    scrape = BeautifulSoup(urllib2.urlopen(url), 'html.parser')
-    list_items = scrape.find('div', attrs={'class': 'list-items'})
-    for child in list_items.children:
-        print parse_title(child[title]), child[rating], child[review]
+class SearchScrape:
+    def __init__(self, url, name):
+        self.url = url
+        self.name = name
 
-scrape_bestbuy(smart_tv)
-print '*************************************************'
-scrape_bestbuy(curved_smart_tv)
+    def scrape(self):
+        self._scrape(self.url)
 
-# create a class
-# use beautifulsoup to gather the data
-# import it into a csv file
-# use python readlines,
-# each day's worth of data set should be one csv file
-# have program iterate through each folder
-# input should be date and have it parsed into folder name
+    def _scrape(self, url):
+        rating = 'data-average-rating'
+        review = 'data-review-count'
+        title = 'data-title'
+        scrape = BeautifulSoup(urllib2.urlopen(url), 'html.parser')
+        list_items = scrape.find('div', attrs={'class': 'list-items'})
+        file_name = datetime.today().strftime("%m%d%y")
+        if not os.path.exists(self.name):
+            os.makedirs(self.name)
+        with open('./{0}/{1}'.format(self.name, file_name), 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            for child in list_items.children:
+                writer.writerow([parse_title(child[title]), child[rating], child[review]])
 
-# class SearchResult:
-#     def __init__(self, *args):
-#
-#     def create_list(self, *args):
-
-# class LinkedList:
-#     def __init__(self, *args):
-#         self.head = self.create_list(*args)
-#
-#     def create_list(self, *args):
-#         last_node = None
-#         for nval in reversed(args):
-#             current_node = Node(nval)
-#             current_node.next = last_node
-#             last_node = current_node
-#         return last_node
-#
-#     def print_ll(self):
-#         self._print_ll(self.head)
-#
-#     def _print_ll(self, node):
-#         if node is None:
-#             return
-#         else:
-#             print(node.val)
-#             self._print_ll(node.next)
-#
-#     def reverse_print_ll(self):
-#         self._reverse_print_ll(self.head)
-#
-#     def _reverse_print_ll(self, node):
-#         if node is None:
-#             return
-#         else:
-#             self._reverse_print_ll(node.next)
-#             print(node.val)
-#
-#     def iterative_print(self):
-#         current_node = self.head
-#         while current_node is not None:
-#             print(current_node.val)
-#             current_node = current_node.next
-#
-#     def pop(self):
-#         if self.head.next is None:
-#             self.head = None
-#             return
-#         else:
-#             self.head = self.head.next
-#             return self.head.val
-#
-#     def push(self, val):
-#         if self.head is None:
-#             self.head = Node(val)
-#             return self.head.val
-#
-#         current_node = self.head
-#
-#         while current_node is not None:
-#             if current_node.next is None:
-#                 current_node.next = Node(val)
-#                 return current_node.next.val
-#             else:
-#                 current_node = current_node.next
-#
-#     def insert(self, val, idx):
-#         current_idx = 0
-#         current_node = self.head
-#         newNode = Node(val)
-#         if idx == 0:
-#             self.head = newNode
-#             self.head.next = current_node
-#             return
-#         while current_idx < idx - 1:
-#             current_node = current_node.next
-#             current_idx += 1
-#         newNode.next = current_node.next
-#         current_node.next = newNode
+import_smart_tv = SearchScrape(smart_tv, 'smart_tv')
+import_smart_tv.scrape()
+import_curved_smart_tv = SearchScrape(curved_smart_tv, 'curved_smart_tv')
+import_curved_smart_tv.scrape()
